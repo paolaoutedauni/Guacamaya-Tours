0850b5b968ef3b0a9b0814ae8061a73019ab0f39
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-10-13 10:57:06 -0400

    Implementacion Inicial de Home
    Faltando el footer, los logos y la implementacion con data JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'guacamaya-tours';
+  title = "guacamaya-tours";
+  itemsPerSlide = 3;
+  singleSlideOffset = false;
+  noWrap = true;
+  slidesChangeMessage = "";
 
-  ngOnInit() {
+  slides = [
+    {
+      image: "../assets/santo_angel.jpg"
+    },
+    {
+      image: "../assets/margarita.jpg"
+    },
+    {
+      image: "../assets/merida.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/7.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/8.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg"
+    },
+    {
+      image:
+        "https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg"
+    }
+  ];
+  ngOnInit() {}
+
+  onSlideRangeChange(indexes: number[]): void {
+    this.slidesChangeMessage = `Slides have been switched: ${indexes}`;
   }
 }
